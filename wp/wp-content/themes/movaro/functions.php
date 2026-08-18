ef1b47f3a70102a6229da411b6d0ea107502270b
Piotr Trybała <trybalapiotr4@gmail.com>
2026-08-18 13:38:08 +0200

    Created header and footer elements

--- wp/wp-content/themes/movaro/functions.php
+++ wp/wp-content/themes/movaro/functions.php
@@ -1,2 +1,58 @@
+<?php
 
+/* global options */
 
+add_filter('show_admin_bar', '__return_false');
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+/* blocks */
+
+/* scripts & styles */
+
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
+
+function dynamicAssetVersion(string $filePath): string
+{
+    return file_exists($filePath)
+        ? (string)filemtime($filePath)
+        : '1.0';
+}
+
+function enqueue_scripts(): void
+{
+    $themeUrl = get_template_directory_uri();
+    $themePath = get_template_directory();
+
+    $cssFile = $themePath . '/dist/app.css';
+    $jsFile = $themePath . '/dist/app.js';
+
+    wp_enqueue_style(
+        'app',
+        $themeUrl . '/dist/app.css',
+        [],
+        dynamicAssetVersion($cssFile)
+    );
+
+    wp_enqueue_script(
+        'app',
+        $themeUrl . '/dist/app.js',
+        ["jquery"],
+        dynamicAssetVersion($jsFile),
+        true
+    );
+}
+
+/* menus */
+
+add_action('init', 'register_menus');
+
+function register_menus()
+{
+    register_nav_menus(
+        array(
+            "header-menu" => __("Menu nagłówka", 'movaro'),
+        )
+    );
+}
+
+/* option pages */
