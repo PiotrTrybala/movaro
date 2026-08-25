1352c28060853f8a3912748760d170fd28e86c5a
Piotr Trybała <trybalapiotr4@gmail.com>
2026-08-25 10:49:07 +0200

    Starting work on reviews swiper

--- wp/wp-content/themes/movaro/blocks/reviews/index.js
+++ wp/wp-content/themes/movaro/blocks/reviews/index.js
@@ -1,1 +1,28 @@
 
+import { Swiper } from 'swiper';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const reviewsSwiper = document.querySelector(".b-reviews__swiper");
+    
+    new Swiper(reviewsSwiper, {
+        wrapperClass: 'b-reviews__swiper-wrapper',
+        slideClass: 'b-reviews__slide',
+
+        modules: [Autoplay],
+        loop: true,
+
+        slidesPerView: 1,
+        
+        speed: 20000,
+        
+        autoplay: {
+            delay: 0,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+        },
+    });
+
+});
--- wp/wp-content/themes/movaro/blocks/reviews/render.php
+++ wp/wp-content/themes/movaro/blocks/reviews/render.php
@@ -6,31 +6,41 @@ $reviews = get_field("reviews") ?? [];
 
 ?>
 
-<section class="container b-reviews">
-    <header class="b-reviews__header">
+<section class="b-reviews">
+    <header class="container b-reviews__header">
         <h2><?= esc_html($title1) ?></h2>
     </header>
-    <div class="b-reviews__content">
-        <?php if ($reviews): ?>
-            <?php foreach ($reviews as $review): ?>
-                <div class="c-review">
-                    <header class="c-review__header">
-                        <?php for ($i = 0; $i < $review['stars']; $i++): ?>
-                            <div class="c-review__header-star">
-                                <?php if ($review_icon): ?>
-                                    <?= wp_get_attachment_image($review_icon['ID'], 'thumbnail') ?>
-                                <?php endif; ?>
-                            </div>
-                        <?php endfor; ?>
-                    </header>
-                    <div class="c-review__content">
-                        <p><?= wp_trim_words(esc_html($review['description'] ?? '')) ?></p>
+
+    <?php if ($reviews): ?>
+        <div class="swiper b-reviews__swiper">
+            <div class="swiper-wrapper b-reviews__swiper-wrapper">
+                <?php for ($j = 0; $j < 1; $j++): ?>
+                    <div class="b-reviews__slide">
+                        <h1>Slide <?= $j ?></h1>
                     </div>
-                    <footer class="c-review__footer">
-                        <h3><?= esc_html($review['username'] ?? '') ?></h3>
-                    </footer>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+    <?php endif; ?>
 </section>
+<!-- <div class="b-reviews__content">
+                        <?php foreach ($reviews as $review): ?>
+                            <div class="c-review">
+                                <header class="c-review__header">
+                                    <?php for ($i = 0; $i < $review['stars']; $i++): ?>
+                                        <div class="c-review__header-star">
+                                            <?php if ($review_icon): ?>
+                                                <?= wp_get_attachment_image($review_icon['ID'], 'thumbnail') ?>
+                                            <?php endif; ?>
+                                        </div>
+                                    <?php endfor; ?>
+                                </header>
+                                <div class="c-review__content">
+                                    <p><?= wp_trim_words(esc_html($review['description'] ?? '')) ?></p>
+                                </div>
+                                <footer class="c-review__footer">
+                                    <h3><?= esc_html($review['username'] ?? '') ?></h3>
+                                </footer>
+                            </div>
+                        <?php endforeach; ?>
+                    </div> -->
