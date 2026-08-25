51604c7f01fa5d076a4c0b22c89d4d9ff1e2fa55
Piotr Trybała <trybalapiotr4@gmail.com>
2026-08-25 10:34:49 +0200

    Implemented Reviews section on Landing page with fixed styling

--- wp/wp-content/themes/movaro/assets/js/index.js
+++ wp/wp-content/themes/movaro/assets/js/index.js
@@ -1,7 +1,7 @@
 import main from "../scss/main.scss";
 
 import sidebar from "../js/sidebar";
-import reviews from "../js/reviews";
 import accordion from "../js/accordion";
 
 import story from '../../blocks/story/index';
+import reviews from '../../blocks/reviews/index';
--- wp/wp-content/themes/movaro/blocks/reviews/render.php
+++ wp/wp-content/themes/movaro/blocks/reviews/render.php
@@ -3,33 +3,34 @@ $title1 = get_field("title_1") ?? '';
 $review_icon = get_field("review_icon") ?? '';
 $reviews = get_field("reviews") ?? [];
 
+
 ?>
 
-<section class="b-reviews container">
-    <div class="b-reviews__gradient"></div>
+<section class="container b-reviews">
     <header class="b-reviews__header">
-        <h2><?= htmlspecialchars($title1) ?></h2>
+        <h2><?= esc_html($title1) ?></h2>
     </header>
     <div class="b-reviews__content">
-        <?php foreach ($reviews as $review): ?>
-            <div class="b-reviews__review">
-                <div class="b-reviews__review-stars">
-                    <?php for ($i = 0; $i < $review['stars']; $i++): ?>
-                        <div class="b-reviews__review-star">
-                            <?php if ($review_icon): ?>
-                                <?= wp_get_attachment_image($review_icon['ID'], 'thumbnail') ?>
-                            <?php endif; ?>
-                        </div>
-                    <?php endfor; ?>
+        <?php if ($reviews): ?>
+            <?php foreach ($reviews as $review): ?>
+                <div class="c-review">
+                    <header class="c-review__header">
+                        <?php for ($i = 0; $i < $review['stars']; $i++): ?>
+                            <div class="c-review__header-star">
+                                <?php if ($review_icon): ?>
+                                    <?= wp_get_attachment_image($review_icon['ID'], 'thumbnail') ?>
+                                <?php endif; ?>
+                            </div>
+                        <?php endfor; ?>
+                    </header>
+                    <div class="c-review__content">
+                        <p><?= wp_trim_words(esc_html($review['description'] ?? '')) ?></p>
+                    </div>
+                    <footer class="c-review__footer">
+                        <h3><?= esc_html($review['username'] ?? '') ?></h3>
+                    </footer>
                 </div>
-                <div class="b-reviews__review-content">
-                    <p><?= htmlspecialchars($review['description']) ?></p>
-                </div>
-                <div class="b-reviews__review-reviewer">
-                    <h3><?= htmlspecialchars($review['username']) ?></h3>
-                </div>
-            </div>
-
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 </section>
